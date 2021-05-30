f4859e8e3c2b77f8c6e635e15493669c8507c8a9
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-05-30 16:15:56 +0300

    Update ProductMapper.java

--- src/main/java/com/boot/cart/services/mapper/ProductMapper.java
+++ src/main/java/com/boot/cart/services/mapper/ProductMapper.java
@@ -8,74 +8,62 @@ import java.util.Set;
 import com.boot.cart.services.dto.ProductDTO;
 import com.boot.services.model.Product;
 
-
 public class ProductMapper {
 
 	public static ProductDTO ProductEntityToDto(Product product) {
-		return new ProductDTO()
-				.setId(product.getId())
-				.setProductName(product.getProductName())
-				.setProductDescription(product.getProductDescription())
-				.setProductPrice(product.getProductPrice())
-				.setProductPhotoLink(product.getProductPhotoLink())
-				.setProductCategory(product.getProductCategory())
+		return new ProductDTO().setId(product.getId()).setProductName(product.getProductName())
+				.setProductDescription(product.getProductDescription()).setProductPrice(product.getProductPrice())
+				.setProductPhotoLink(product.getProductPhotoLink()).setProductCategory(product.getProductCategory())
 				.setProductStock(product.getProductStock());
 	}
 
 	public static Product DtoToProductEntity(ProductDTO productDto) {
-		return new Product()
-				.setId(productDto.getId())
-				.setProductName(productDto.getProductName())
-				.setProductDescription(productDto.getProductDescription())
-				.setProductPrice(productDto.getProductPrice())
+		return new Product().setId(productDto.getId()).setProductName(productDto.getProductName())
+				.setProductDescription(productDto.getProductDescription()).setProductPrice(productDto.getProductPrice())
 				.setProductPhotoLink(productDto.getProductPhotoLink())
-				.setProductCategory(productDto.getProductCategory())
-				.setProductStock(productDto.getProductStock());		
+				.setProductCategory(productDto.getProductCategory()).setProductStock(productDto.getProductStock());
 	}
-	
-	public static Product updateDtoToProductEntity(Product product,ProductDTO productDto) {
-		return  product
-				.setId(productDto.getId())
-				.setProductName(productDto.getProductName())
-				.setProductDescription(productDto.getProductDescription())
-				.setProductPrice(productDto.getProductPrice())
+
+	public static Product updateDtoToProductEntity(Product product, ProductDTO productDto) {
+		return product.setId(productDto.getId()).setProductName(productDto.getProductName())
+				.setProductDescription(productDto.getProductDescription()).setProductPrice(productDto.getProductPrice())
 				.setProductPhotoLink(productDto.getProductPhotoLink())
-				.setProductCategory(productDto.getProductCategory())
-				.setProductStock(productDto.getProductStock());		
+				.setProductCategory(productDto.getProductCategory()).setProductStock(productDto.getProductStock());
 	}
-	public static List<ProductDTO> productEntityToDtoList(List<Product> productList){
-		
+
+	public static List<ProductDTO> productEntityToDtoList(List<Product> productList) {
+
 		List<ProductDTO> productDTOList = new ArrayList<>();
 
-		productList.stream().forEach(p -> productDTOList.add(ProductMapper.ProductEntityToDto(p)));	
-				
+		productList.stream().forEach(p -> productDTOList.add(ProductMapper.ProductEntityToDto(p)));
+
 		return productDTOList;
 	}
-	
-	public static List<Product> dtoToProductEntityList(List<ProductDTO> productDTOList){
-		
+
+	public static List<Product> dtoToProductEntityList(List<ProductDTO> productDTOList) {
+
 		List<Product> productList = new ArrayList<>();
-			
-		productDTOList.stream().forEach(pDTO -> productList.add(ProductMapper.DtoToProductEntity(pDTO)));	
-		
-		return 	productList;
+
+		productDTOList.stream().forEach(pDTO -> productList.add(ProductMapper.DtoToProductEntity(pDTO)));
+
+		return productList;
 	}
-	
-	public static Set<ProductDTO> productEntityToDtoList(Set<Product> productList){
-		
+
+	public static Set<ProductDTO> productEntityToDtoList(Set<Product> productList) {
+
 		Set<ProductDTO> productDTOList = new HashSet<>();
 
-		productList.stream().forEach(p -> productDTOList.add(ProductMapper.ProductEntityToDto(p)));	
-				
+		productList.stream().forEach(p -> productDTOList.add(ProductMapper.ProductEntityToDto(p)));
+
 		return productDTOList;
 	}
-	
-	public static Set<Product> dtoToProductEntityList(Set<ProductDTO> productDTOList){
-		
+
+	public static Set<Product> dtoToProductEntityList(Set<ProductDTO> productDTOList) {
+
 		Set<Product> productList = new HashSet<>();
-			
-		productDTOList.stream().forEach(pDTO -> productList.add(ProductMapper.DtoToProductEntity(pDTO)));	
-		
-		return 	productList;
+		if (productDTOList != null) {
+			productDTOList.stream().forEach(pDTO -> productList.add(ProductMapper.DtoToProductEntity(pDTO)));
+		}
+		return productList;
 	}
 }
